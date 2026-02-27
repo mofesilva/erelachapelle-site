fd4aa3b02bac3e7d3e16e25250c57411ca029603
mofesilva <mofesilva.dev@gmail.com>
2026-02-27 18:38:10 -0300

    feat: add FacebookIcon component for social media integration

---
+++ src/_components/icons/FacebookIcon.tsx
@@ -0,0 +1,16 @@
+import type { SVGProps } from "react";
+
+export function FacebookIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            aria-hidden="true"
+            {...props}
+        >
+            <path d="M9.198 21.5h4v-8.01h3.604l.396-3.98h-4V7.5a1 1 0 0 1 1-1h3v-4h-3a5 5 0 0 0-5 5v2.01h-2l-.396 3.98h2.396v8.01Z" />
+        </svg>
+    );
+}
