53aacebf7fc9594637eb042f58eb422d54e75976
mofesilva <mofesilva.dev@gmail.com>
2026-03-12 17:39:05 -0300

    feat: add leadership module index

---
+++ src/lib/leadership/index.ts
@@ -0,0 +1,4 @@
+export type { LeadershipMember } from "./model";
+export { mapLeader, mapLeaders } from "./mapper";
+export { leadershipCollection, LEADERSHIP_COLLECTION } from "./collection";
+export { getLeadershipTeam, createLeader, updateLeader, deleteLeader } from "./service";
