a5a21c069841a58b601bebf036c3de511463abf0
mofesilva <mofesilva.dev@gmail.com>
2026-03-12 17:38:59 -0300

    feat: add locations module index

---
+++ src/lib/locations/index.ts
@@ -0,0 +1,4 @@
+export type { Location } from "./model";
+export { mapLocation, mapLocations } from "./mapper";
+export { locationsCollection, LOCATIONS_COLLECTION } from "./collection";
+export { getLocations, getLocationById, createLocation, updateLocation, deleteLocation } from "./service";
