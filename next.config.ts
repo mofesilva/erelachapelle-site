75680f8d7500615caf9e292c75bada2ce6c1520c
mofesilva <mofesilva.dev@gmail.com>
2026-03-12 11:50:46 -0300

    config: allow images.unsplash.com in next.config

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**.fbcdn.net",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
