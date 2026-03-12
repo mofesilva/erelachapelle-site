9c62b32f0271ab063717e9f37474b9c02a25ad57
mofesilva <mofesilva.dev@gmail.com>
2026-03-12 17:36:36 -0300

    config: add cappuccino sdk transpile and devel api hostname

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,16 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  transpilePackages: ["@cappuccino/web-sdk"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cappuccino.dzign-e.app",
+      },
+      {
+        protocol: "https",
+        hostname: "cappuccino.devel.dzign-e.app",
       },
       {
         protocol: "https",
