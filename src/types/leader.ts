e2e8d2a0de36e24dfe100f5bb0eba2ca1b1d6ca0
mofesilva <mofesilva.dev@gmail.com>
2026-04-29 15:46:56 -0300

    update: leader type definitions

--- src/types/leader.ts
+++ src/types/leader.ts
@@ -2,9 +2,11 @@ import type { MultilingualText } from "./common";
 
 export interface LeadershipMember {
   _id: string;
+  slug: string;
   fullName: string;
   role: MultilingualText;
   bio: MultilingualText;
+  fullBio: MultilingualText;
   photoUrl?: string;
   email?: string;
   ministryAreas: string[];
