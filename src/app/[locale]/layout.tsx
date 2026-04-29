571d430d116c7a7cd342a023b7e628a05cb7d0eb
mofesilva <mofesilva.dev@gmail.com>
2026-04-29 15:46:47 -0300

    update: locale layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     default: "Église Réformée Évangélique La Chapelle",
     template: "%s | Église Réformée Évangélique La Chapelle",
   },
-  description: "Communauté chrétienne dans les Cévennes",
+  description: "Rassemblés autour de la Parole de Dieu",
   metadataBase: new URL("https://erelachapelle.fr"),
   openGraph: {
     type: "website",
