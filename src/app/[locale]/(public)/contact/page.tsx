914a2a76292676ad7bdaa24abf4a3ee3ec67afd2
mofesilva <mofesilva.dev@gmail.com>
2026-05-07 12:12:16 -0300

    feat: update contact page

--- src/app/[locale]/(public)/contact/page.tsx
+++ src/app/[locale]/(public)/contact/page.tsx
@@ -19,6 +19,7 @@ export default async function ContactPage() {
     <main>
       <SecondaryHeroSection
         title={t("title")}
+        bgImage="/images/inside-church.jpg"
       />
 
       {/* ── Section 1: Formulário de contato ────────────────────── */}
