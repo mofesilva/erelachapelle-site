bb787e7d67f716e74fc1eb6df10d217f46e93c3e
mofesilva <mofesilva.dev@gmail.com>
2026-08-19 17:21:54 -0300

    chore: update src/app/[locale]/(public)/page.tsx

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -3,7 +3,7 @@ import { getTranslations } from "next-intl/server";
 import { HeroSection } from "../_components/HeroSection";
 import { FaithStatementSection } from "../_components/FaithStatementSection";
 import { GatheringSection } from "../_components/GatheringSection";
-import { EventsComingSoonBanner } from "../_components/EventsComingSoonBanner";
+import { EventsPreviewSection } from "../_components/EventsPreviewSection";
 import { CommunitySection } from "../_components/CommunitySection";
 import { NewsletterSection } from "../_components/NewsletterSection";
 import { churchJsonLd } from "@/lib/structured-data";
@@ -38,7 +38,7 @@ export default async function HomePage() {
       <FaithStatementSection />
       <GatheringSection />
       <CommunitySection />
-      <EventsComingSoonBanner />
+      <EventsPreviewSection />
       <NewsletterSection />
     </>
   );
